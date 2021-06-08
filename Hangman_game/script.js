5074292680446cd88ccef2cb0607b69602384ab8
Pietro Lauria <ptr.lauria@gmail.com>
2021-06-08 10:31:05 +0200

    Hangman

---
+++ Hangman_game/script.js
@@ -0,0 +1,5 @@
+const wordEl = document.getElementById('word');
+const wrongLettersEl = document.getElementById('wrong-letters');
+const playAgainBtn = document.getElementById('play-button');
+const popup = document.getElementById('popup-container');
+const notification = document.getElementById('notification-container');
