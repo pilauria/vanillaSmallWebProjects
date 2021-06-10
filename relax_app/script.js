20470e34d00f6c7a152d834136774412f70df181
Pietro Lauria <ptr.lauria@gmail.com>
2021-06-10 08:26:36 +0200

    Breathe_App

---
+++ relax_app/script.js
@@ -0,0 +1,27 @@
+const container = document.getElementById('container');
+const text = document.getElementById('text');
+
+const totalTime = 7500;
+const breatheTime = (totalTime / 5) * 2;
+const holdTime = totalTime / 5;
+
+breathAnimation();
+
+function breathAnimation() {
+  // console.log('Breathe In!');
+  text.innerText = 'Breathe In!';
+  container.className = 'container grow';
+
+  setTimeout(() => {
+    // console.log('Hold');
+    text.innerText = 'Hold';
+
+    setTimeout(() => {
+      // console.log('Breathe Out!');
+      text.innerText = 'Breathe Out!';
+      container.className = 'container shrink';
+    }, holdTime);
+  }, breatheTime);
+}
+
+setInterval(breathAnimation, totalTime);
