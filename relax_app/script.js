f6d1236b69de32f880b83b7f06a1d4f7ce8c3552
Pietro Lauria <ptr.lauria@gmail.com>
2021-06-14 17:30:40 +0200

    Final buttons

--- relax_app/script.js
+++ relax_app/script.js
@@ -2,14 +2,16 @@ const container = document.getElementById('container');
 const text = document.getElementById('text');
 const start = document.querySelector('#start');
 const stop = document.querySelector('#stop');
+const pointerContainer = document.querySelector('.pointer-container');
+let intervalAnimation = null;
 
 const totalTime = 7500;
 const breatheTime = (totalTime / 5) * 2;
 const holdTime = totalTime / 5;
 
-breathAnimation();
+// breathAnimation();
 
-function breathAnimation() {
+let breathAnimation = function () {
   // console.log('Breathe In!');
   text.innerText = 'Breathe In!';
   container.className = 'container grow';
@@ -24,25 +26,24 @@ function breathAnimation() {
       container.className = 'container shrink';
     }, holdTime);
   }, breatheTime);
-}
+};
 
-setInterval(breathAnimation, totalTime);
-
-////////////////////////////////////
-// Right now the app start as the page is loaded. I want to add 'start/stop' buttons.
-// I need to add 2 event listeren (1 for the 'start' button', 1 for the 'stop' button).
+// setInterval(breathAnimation, totalTime);
 
 // function stopBreathe() {
 //   text.innerText = 'Breathe In!';
 //   container.className = 'container';
 // }
 
-// start.addEventListener('click', () => {
-//   pointerContainer.className = 'pointerContainer animation';   <=== I don't know how to change this class
-//   breathAnimation();
-//   setInterval(breathAnimation, totalTime);
-// });
+start.addEventListener('click', () => {
+  pointerContainer.className = 'pointer-container animation';
+  breathAnimation();
+  intervalAnimation = setInterval(breathAnimation, totalTime);
+});
 
-// stop.addEventListener('click', () => {
-//   setInterval(breathAnimation, 0);
-// });
+stop.addEventListener('click', () => {
+  pointerContainer.className = 'pointer-container';
+  container.className = 'container';
+  text.innerText = 'Press start';
+  clearInterval(intervalAnimation);
+});
